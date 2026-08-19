ba07f5a4bba5caea045df0e13aa1c613d96f581d
srcm333 <srcm333@gmail.com>
2026-08-20 01:59:18 +0530

    jenkins

--- vars/nodejsEKSPipeline.groovy
+++ vars/nodejsEKSPipeline.groovy
@@ -101,7 +101,7 @@ def call (Map configMap){
                                     -H "Accept: application/vnd.github+json" \
                                     -H "Authorization: Bearer ${GH_TOKEN}" \
                                     -H "X-GitHub-Api-Version: 2026-03-10" \
-                                    "https://api.github.com/repos/${REPO}/dependabot/alerts?state=open" \
+                                    "https://api.github.com/${REPO}/dependabot/alerts?state=open" \
                                     -o alerts.json
         
                                     echo "---- Open Dependabot Alerts ----"
