3feca7c2066b2b9a8ac158316e9c413c896185e3
srcm333 <srcm333@gmail.com>
2026-08-20 01:05:02 +0530

    jenkins

--- vars/nodejsEKSPipeline.groovy
+++ vars/nodejsEKSPipeline.groovy
@@ -89,34 +89,40 @@ def call (Map configMap){
             stage('Check Dependabot Alerts') {
                 steps {
                     withCredentials([string(credentialsId: 'github-token', variable: 'GH_TOKEN')]) {
-                        sh '''
-                            set -e
-
-                            REPO='${org}/${component}'
-
-                            curl -s -L \
-                            -H "Accept: application/vnd.github+json" \
-                            -H "Authorization: Bearer ${GH_TOKEN}" \
-                            -H "X-GitHub-Api-Version: 2026-03-10" \
-                            "https://api.github.com/repos/${REPO}/dependabot/alerts?state=open" \
-                            -o alerts.json
-
-                            echo "---- Open Dependabot Alerts ----"
-                            jq -r '.[] | "\\(.number)\\t\\(.security_vulnerability.severity)\\t\\(.dependency.package.name)\\t\\(.security_advisory.ghsa_id)"' alerts.json
-
-                            HIGH_CRITICAL_COUNT=$(jq '[.[] | select(.security_vulnerability.severity == "high" or .security_vulnerability.severity == "critical")] | length' alerts.json)
-
-                            echo "High/Critical alert count: ${HIGH_CRITICAL_COUNT}"
-
-                            if [ "$HIGH_CRITICAL_COUNT" -gt 0 ]; then
-                                echo "❌ Found ${HIGH_CRITICAL_COUNT} High/Critical severity dependency alert(s). Failing build."
-                                utils.updateCommitStatus("failure", "library scan failed", "library-scan")
-                                exit 1
-                            else
-                                echo "✅ No High/Critical dependency alerts found."
-                                utils.updateCommitStatus("success", "library scan success", "library-scan")
-                            fi
-                        '''
+                        try{
+                            sh '''
+                                set -e
+    
+                                REPO='${org}/${component}'
+    
+                                curl -s -L \
+                                -H "Accept: application/vnd.github+json" \
+                                -H "Authorization: Bearer ${GH_TOKEN}" \
+                                -H "X-GitHub-Api-Version: 2026-03-10" \
+                                "https://api.github.com/repos/${REPO}/dependabot/alerts?state=open" \
+                                -o alerts.json
+    
+                                echo "---- Open Dependabot Alerts ----"
+                                jq -r '.[] | "\\(.number)\\t\\(.security_vulnerability.severity)\\t\\(.dependency.package.name)\\t\\(.security_advisory.ghsa_id)"' alerts.json
+    
+                                HIGH_CRITICAL_COUNT=$(jq '[.[] | select(.security_vulnerability.severity == "high" or .security_vulnerability.severity == "critical")] | length' alerts.json)
+    
+                                echo "High/Critical alert count: ${HIGH_CRITICAL_COUNT}"
+    
+                                if [ "$HIGH_CRITICAL_COUNT" -gt 0 ]; then
+                                    echo "❌ Found ${HIGH_CRITICAL_COUNT} High/Critical severity dependency alert(s). Failing build."
+                                    exit 1
+                                else
+                                    echo "✅ No High/Critical dependency alerts found."
+                                fi
+                            '''
+                            utils.updateCommitStatus("success", "library scan success", "library-scan")
+                        }
+                        catch (Exception e){
+                            utils.updateCommitStatus("failure", "library scan failed", "library-scan")
+                            throw e
+                        }
+                        
                     }
                 }
             }
